24ca523365f084e204c8d4f7f827ed06e0179853
Iqbal66x <103598357+Iqbal66x@users.noreply.github.com>
2023-05-29 18:04:06 +0700

    Sleep_health_and_lifestyle_dataset_finished.R
    data analysis was performed on a number of variables related to individual health and lifestyle

--- Sleep_health_and_lifestyle_dataset_finished.R
+++ Sleep_health_and_lifestyle_dataset_finished.R
@@ -14,37 +14,45 @@ Sleep <- read_csv("C:/Users/iqbal/Downloads/Sleep_health_and_lifestyle_dataset.c
 View(sleep)
 
 ### correlation between sleep time and sleep quality
+# Adequate sleep and good quality sleep are very important for physical and mental health
+# Sleep time affects sleep quality
 
 ggplot(data = sleep) + geom_point(mapping = aes(x = QualityofSleep, y = SleepDuration)) +
   geom_smooth(mapping = aes(x = QualityofSleep, y = SleepDuration))
 
 ### relationship between Physical Activity Levels and Stress Levels
+# people who have more physical activity have less stress levels
+# It is generally recognized that physical activity can help reduce stress
 
-ggplot(data = sleep) + geom_point(mapping = aes(x = StressLevel, y = PhysicalActivityLevel)) +
-  geom_smooth(mapping = aes(x = StressLevel, y = PhysicalActivityLevel))
+ggplot(data = sleep) + geom_point(mapping = aes(x = StressLevel, y = PhysicalActivityLevel))
 
 ### correlation between sleep time and sleep quality, sleep disorder
+# sleep time and sleep quality if you have a sleep disorder
+# People with sleep disorders have poor sleep quality and sleep duration
 
-ggplot(data = sleep) + geom_point(mapping = aes(x = QualityofSleep, y = SleepDuration)) +
-  geom_smooth(mapping = aes(x = QualityofSleep, y = SleepDuration,color=SleepDisorder))
+ggplot(data = sleep) + geom_point(mapping = aes(x = QualityofSleep, y = SleepDuration))
 
 ### correlation between Quality of Sleep and Heart Rate
+# people who have better quality sleep have lower heart rates 
+# This normal range is usually between 60 and 100 beats per minute at rest
 
-ggplot(data = sleep) + geom_point(mapping = aes(x = QualityofSleep, y = HeartRate)) +
-  geom_smooth(mapping = aes(x = QualityofSleep, y = HeartRate))
+ggplot(data = sleep) + geom_smooth(mapping = aes(x = QualityofSleep, y = HeartRate))
+
 
 ### correlation between Quality of Sleep and Stress Level
+# People with good quality sleep have lower stress levels
+# High stress levels can negatively impact sleep quality, whereas poor sleep can increase stress levels
 
 ggplot(data = sleep) + geom_point(mapping = aes(x = QualityofSleep, y = StressLevel)) +
   geom_smooth(mapping = aes(x = QualityofSleep, y = StressLevel))
-              
-              
-### correlation between Physical Activity Level and Daily Steps,BMI Category
+                            
+### correlation between Physical Activity Level and Daily Steps on body weight
+# Heavier people have lower levels of physical activity and daily stepss
 
 ggplot(data = sleep) + geom_point(mapping = aes(x = PhysicalActivityLevel, y = DailySteps)) +
   geom_smooth(mapping = aes(x = PhysicalActivityLevel, y = DailySteps)) + facet_wrap(~BMICategory)
 
-### Quality Sleep of Occupation 
+### sleep quality from various occupations
 
 average_quality <- mean(sleep$QualityofSleep)
 
@@ -57,5 +65,7 @@ average <- aggregate(QualityofSleep ~ Occupation, data = sleep, FUN = mean)
 ggplot(data = average) + geom_col(mapping = aes(x = QualityofSleep, y = Occupation))
 
 ### quality sleep by age
+# Sleep quality generally tends to change with age
+# women have higher sleep quality
 
-ggplot(data = sleep) + geom_smooth(mapping = aes(x = QualityofSleep, y = Age,color = Gender))
+ggplot(data = sleep) + geom_smooth(mapping = aes(x = Age, y = QualityofSleep,color = Gender))
